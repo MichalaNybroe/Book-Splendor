6f952e4154759442cf33caf61118e1e290a4a4ce
silke <silk2918@stud.kea.dk>
2022-12-14 23:28:52 +0100

    contact form styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,32 @@
 <script>
+  
+  //why?????????????
+  import { Router, Link, Route } from "svelte-navigator";
+
+  import Home from "./pages/home/Home.svelte";
+  import Registration from "./pages/registration/Registration.svelte";
+  import Login from "./pages/login/Login.svelte";
+  import Contact from "./pages/contact/Contact.svelte";
 </script>
 
+<Router>
+  <nav>
+    <h1>Book-Splendor</h1>
+    <Link to="/">Home</Link>
+    <Link to="/login">Login</Link>
+    <Link to="/registration">Registration</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="/login"><Login /></Route>
+    <Route path="/registration"><Registration /></Route>
+    <Route path="/contact"><Contact /></Route>
+</div>
+</Router>
+
 <main>
-  <h1>Book-Splendor</h1>
+ 
 </main>
 
 <style>
---
+++ client/src/pages/contact/Contact.svelte
@@ -0,0 +1,190 @@
+<script>
+    import { onMount } from "svelte";
+    import { BASE_URL } from "../../store/globals.js"
+    
+
+    onMount(() => {
+        const email = document.getElementById("email")
+        const message = document.getElementById("message")
+        const contactForm = document.getElementById("contactForm")
+        contactForm.addEventListener("submit", (event) => {
+            event.preventDefault()
+            const body = {
+                email: email.value,
+                message: message.value
+            }
+            return fetch(`${$BASE_URL}/contact`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            }).then((response) => {
+                    if (response.ok) {
+                        console.log("OK. Message sent.")
+                    } else {
+                        console.log("Error. Message not sent.")
+                    }
+                })
+        })
+    })
+</script>
+
+<h3>Any questions? Contact us here</h3>
+
+
+<div id="container">
+    <h1>&bull; Keep in Touch &bull;</h1>
+  
+    <form action="#" method="post" id="contact_form">
+      <div class="name">
+        <label for="name"></label>
+        <input type="text" placeholder="My name is" name="name" id="name_input" required>
+      </div>
+      <div class="email">
+        <label for="email"></label>
+        <input type="email" placeholder="My e-mail is" name="email" id="email_input" required>
+      </div>
+      <div class="telephone">
+        <label for="name"></label>
+        <input type="text" placeholder="My number is" name="telephone" id="telephone_input" required>
+      </div>
+      <div class="subject">
+        <label for="subject"></label>
+        <select placeholder="Subject line" name="subject" id="subject_input" required>
+          <option disabled hidden selected>Subject line</option>
+          <option>I'd like to start a project</option>
+          <option>I'd like to ask a question</option>
+          <option>I'd like to make a proposal</option>
+        </select>
+      </div>
+      <div class="message">
+        <label for="message"></label>
+        <textarea name="message" placeholder="I'd like to chat about" id="message_input" cols="30" rows="5" required></textarea>
+      </div>
+      <div class="submit">
+        <input type="submit" value="Send Message" id="form_button" /> 
+      </div>
+    </form><!-- // End form -->
+  </div><!-- // End #container -->
+
+
+<style>
+    
+@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
+
+
+input, select, textarea {
+  color: #5A5A5A;
+  font: inherit;
+  margin: 0;
+}
+
+input {
+  line-height: normal;
+}
+
+textarea {
+  overflow: auto;
+}
+
+#container {
+  border: solid 3px #474544;
+  max-width: 768px;
+  margin: 60px auto;
+  position: relative;
+}
+
+form {
+  padding: 37.5px;
+  margin: 50px 0;
+}
+
+h1 {
+  color: #474544;
+  font-size: 32px;
+  font-weight: 700;
+  letter-spacing: 7px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.email {
+	float: right;
+	width: 45%;
+}
+
+input[type='text'], [type='email'], select, textarea {
+	background: none;
+  border: none;
+	border-bottom: solid 2px #474544;
+	color: #474544;
+	font-size: 1.000em;
+  font-weight: 400;
+  letter-spacing: 1px;
+	margin: 0em 0 1.875em 0;
+	padding: 0 0 0.875em 0;
+  text-transform: uppercase;
+	width: 100%;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	-ms-box-sizing: border-box;
+	-o-box-sizing: border-box;
+	box-sizing: border-box;
+	-webkit-transition: all 0.3s;
+	-moz-transition: all 0.3s;
+	-ms-transition: all 0.3s;
+	-o-transition: all 0.3s;
+	transition: all 0.3s;
+}
+
+input[type='text']:focus, [type='email']:focus, textarea:focus {
+	outline: none;
+	padding: 0 0 0.875em 0;
+}
+
+.message {
+	float: none;
+}
+
+.name {
+	float: left;
+	width: 45%;
+}
+
+.subject {
+  width: 100%;
+}
+
+.telephone {
+  width: 100%;
+}
+
+textarea {
+	line-height: 150%;
+	height: 150px;
+	resize: none;
+  width: 100%;
+}
+
+
+#form_button {
+  background: none;
+  border: solid 2px #474544;
+  color: #474544;
+  cursor: pointer;
+  display: inline-block;
+  font-family: 'Helvetica', Arial, sans-serif;
+  font-size: 0.875em;
+  font-weight: bold;
+  outline: none;
+  padding: 20px 35px;
+  text-transform: uppercase;
+}
+
+#form_button:hover {
+  background: #474544;
+  color: #F2F3EB;
+}
+
+/*
+Styling by David Fitas on https://codepen.io/dfitzy/pen/VepqMq */
+</style>
