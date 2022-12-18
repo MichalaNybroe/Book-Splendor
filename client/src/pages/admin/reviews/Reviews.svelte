7bd79f2e4a3f69eb0371e41bda53d40dc7f6af77
Michala Nybroe <michalanim@gmail.com>
2022-12-18 11:02:19 +0100

    basic navigation check on admin pages and profile page

---
+++ client/src/pages/admin/reviews/Reviews.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { user } from "../../../store/auth.js"
+    import { useNavigate } from "svelte-navigator"
+
+    
+    if($user.admin !== true) {
+        const navigate = useNavigate()
+
+        navigate("/")
+    }
+</script>
---
+++ client/src/pages/profile/Profile.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { useNavigate } from "svelte-navigator"
+    import { user } from "../../store/auth.js"
+
+    
+    if($user.admin === true || $user === null) {
+        const navigate = useNavigate()
+
+        navigate("/")
+    }
+</script>
