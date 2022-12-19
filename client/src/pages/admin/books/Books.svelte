b5983808f8927499093d8971da8f9ba92f0161e7
silke <silk2918@stud.kea.dk>
2022-12-19 09:43:36 +0100

    books toastr path

--- client/src/pages/admin/books/Books.svelte
+++ client/src/pages/admin/books/Books.svelte
@@ -5,8 +5,6 @@
     import { user } from "../../../store/auth"
     import * as Toastr from "toastr"
     import '../../../../node_modules/toastr/build/toastr.css'
-    //import '../../../node_modules/toastr/build/toastr.css'
-    //har vi node modules liggende forskellige steder? could become a problem. this path doesnt work for me
 
     if($user.admin !== true) {
         const navigate = useNavigate()
