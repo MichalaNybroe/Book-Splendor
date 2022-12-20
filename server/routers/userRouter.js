8a0f62148c18e6a3894b7da87871fbb900df54a4
Michala Nybroe <michalanim@gmail.com>
2022-12-20 09:42:59 +0100

    patch user: on color, picture and username, delete user: admin and user with same id as one to change. sign up changed with default picture and color + not logged in immediately

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -4,12 +4,27 @@ const router = Router()
 import db from "../database/connection.js"
 
 function adminGuard(req, res, next) {
-    if (req.session.IsLoggedIn !== true) {
-        return res.status(401).send({ message: "Not logged in."})
-    }
+    checkLoggedInStatus()
 
     if (req.session.admin !== true) {
-        return res.status(401).send({ message: "Not authorized."})
+        return res.status(401).send({ message: "Not authorized." })
+    }
+    next()
+}
+
+function userGuard(req, res, next) {
+    checkLoggedInStatus()
+
+    if (req.session.id !== req.params.id) {
+        return res.status(401).send({ message: "Not authorized." })
+    }
+
+    next()
+}
+
+function checkLoggedInStatus(req, res, next) {
+    if (req.session.IsLoggedIn !==true) {
+        return res.status(401).send({ message: "Not signed in. " })
     }
     next()
 }
@@ -23,18 +38,35 @@ router.get("/api/users", adminGuard, async (req, res) => {
     }
 })
 
-//enable change of  username
-
-
-//enable deletion of own user
-
-router.delete("/api/users/:id", adminGuard, async (req, res) => {
-    const result = await db.query("DELETE FROM users WHERE users.id=?;", [req.params.id])
-    if (result === undefined) {
-      res.status(404).send({ data: undefined, message: `No book with ${req.params.id} id`})
-    } else {
-        res.send({ data: result })
+router.patch("/api/users/:id"), userGuard, async (req, res) => {
+    if (req.body.user_name) {
+        db.query("UPDATE users SET user_name = ? WHERE id=?", req.body.user_name, req.session.id)
+    } else if (req.body.color) {
+        db.query("UPDATE users SET color = ? WHERE id=?", req.body.color, req.session.id)
+    } else if (req.body.picture_number) {
+        db.query("UPDATE users SET picture_number = ? WHERE id=?", req.body.picture_number, req.session.id)
     }
+}
+
+router.delete("/api/users/:id", checkLoggedInStatus, async (req, res) => {
+    if (req.session.id === req.params.id) {
+        const result = await db.query("DELETE FROM users WHERE users.id=?;", [req.params.id])
+        
+        if (result === undefined) {
+          res.status(404).send({ data: undefined, message: `No user with this id`})
+        } else {
+            res.send({ data: result })
+        }
+    } else {
+        adminGuard()
+        const result = await db.query("DELETE FROM users WHERE users.id=?;", [req.params.id])
+        
+        if (result === undefined) {
+          res.status(404).send({ data: undefined, message: `No user with ${req.params.id} id`})
+        } else {
+            res.send({ data: result })
+        }
+    } 
 })
 
 
