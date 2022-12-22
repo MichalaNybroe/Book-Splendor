6ab663ff9ad4e0f5bf0d6bfd5928aed6af5bd3c5
Silke Marie <silk2918@stud.kea.dk>
2022-12-22 13:35:27 +0100

    list of users

--- client/src/pages/admin/users/Users.svelte
+++ client/src/pages/admin/users/Users.svelte
@@ -1,6 +1,9 @@
 <script>
     import { user } from "../../../store/auth.js"
     import { useNavigate } from "svelte-navigator"
+    import { BASE_URL } from "../../../store/globals.js";
+    import * as Toastr from "toastr";
+    import { onMount } from "svelte";
 
     
     if($user?.admin !== true) {
@@ -8,4 +11,38 @@
 
         navigate("/")
     }
+
+    let users = []
+
+    async function fetchUsers() {
+        const response = await fetch(`${$BASE_URL}/api/users`, {
+            credentials: "include"
+        })
+
+        if(response.ok) {
+            const data = await response.json()
+            users = data.data
+        } else {
+            Toastr.warning("Unable to retrieve users.")
+        }
+    }
+
+    onMount(fetchUsers)
+
+    
+
+
 </script>
+
+
+<table>
+    <tr>
+        <th></th>
+    </tr>
+</table>
+
+<!--
+{#each users as user}
+    <User user={user} users={users}></User>
+{/each}
+-->
