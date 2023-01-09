f66b1a2eecc9f6a9a10b10b35ff4c33b8c709063
Michala Nybroe <michalanim@gmail.com>
2023-01-09 15:13:22 +0100

    rework profile editing and now username patch works

--- client/src/pages/profile/Profile.svelte
+++ client/src/pages/profile/Profile.svelte
@@ -3,8 +3,9 @@
     import { user } from '../../store/auth.js'
     import * as Toastr from 'toastr'
     import '../../../node_modules/toastr/build/toastr.css'
-    import ColorChangesWidget from '../../components/ColorChangesWidget/ColorChangesWidget.svelte'
     import { BASE_URL } from '../../store/globals.js'
+    import Button from '../../components/Button.svelte'
+    import { prevent_default } from 'svelte/internal';
     
     
     if ($user === null || $user.admin === true) {
@@ -13,9 +14,9 @@
         navigate('/')
     }
 
-    async function saveColor() {
-        const body = { color: $user.color }
+    let updateMode = false
 
+    async function patchUser(body, success, unsuccess) {
         try {
             const response = await fetch(`${$BASE_URL}/api/users/${$user.id}`, {
                 method: 'PATCH',
@@ -25,22 +26,50 @@
             })
 
             if(!response.ok) {
-                Toastr.warning('Banner color update was unsuccessfull.')
+                Toastr.warning(unsuccess)
                 return
             }
-            Toastr.success('Banner color updated.')
+            Toastr.success(success)
         } catch {
-            Toastr.error('Unable to update banner color. Try again later.')
+            Toastr.error('Unable to update user. Try again later.')
         }
+    }
+
+    function saveColor() {
+        const body = { color: $user.color }
+
+        patchUser(body, 'Banner color updated.', 'Banner color update was unsuccessfull.')
+    }
+
+    async function updateUserName(event) {
+        event.preventDefault()
+        const body = { user_name: $user.user_name }
+
+        patchUser(body, 'Username sucessfully updated.', 'Unsucessfull attempt at updating username.')
+    }
+
+    function enterEditMode() {
+        updateMode = true
+    }
+
+    function exitEditMode() {
+        updateMode = false
     }
 </script>
 
 <div id="profilebanner" style="background-color: {$user.color};color:white">
-    <img id="profilePicture" alt="User image.">
+    <img id="profilePicture" alt="Profile Picture.">
 
     <h3 id="username">{$user?.user_name}</h3>
-    <input type="color" bind:value={$user.color} style="height: 50px;" on:change|preventDefault={saveColor} id="colorInp">
 </div>
+
+{#if updateMode === true} 
+    <input type="color" bind:value={$user.color} style="height: 50px;" on:change|preventDefault={saveColor} id="colorInp">
+    <input type="text" bind:value={$user.user_name} on:change|preventDefault={updateUserName}>
+    <Button on:click={()=> exitEditMode()}>Exit Update Mode</Button>
+{:else}
+    <Button on:click={()=> enterEditMode()}>Edit Profile</Button>
+{/if}
 
 <div id="toReadList">
 
@@ -50,17 +79,10 @@
 
 </div>
 
+<div id="reviewList">
 
-<p>mark as</p>
-<p>read</p>
-<p>want to read</p>
-<p>reviews</p>
+</div>
 
-<!--<input bind:value={colorValue} type="color">
-<button on:click={sendColor}>Choose color</button>-->
-
-
-<ColorChangesWidget />
 
 <Router primary={false}>
     <Link to="/invite">Invite a friend?</Link>
